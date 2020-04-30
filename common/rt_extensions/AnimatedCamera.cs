b28f8376f6e60f9342e822bf266d04c235a65b48
Michal Töpfer <michal.topfer@gmail.com>
2020-04-30 12:26:38 +0200

    AnimatedCamera init

---
+++ common/rt_extensions/AnimatedCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using MathSupport;
+using OpenTK;
+
+namespace Rendering
+{
+  class AnimatedCamera : ICamera, ITimeDependent
+  {
+    // TODO: read keyframes, save them
+    // TODO: GetRay should interpolate the keyframes, apply them to IAnimatableCamera inside and call GetRay on it
+  }
+
+  class AnimatableStaticCamera : StaticCamera, IAnimatableCamera
+  {
+    public virtual string[] GetParamNames ()
+    {
+      return new string[] { "position", "direction" };
+    }
+
+    public virtual void ApplyParams (Dictionary<string, object> p)
+    {
+      // TODO: handle missing keys in dictionary
+      center = (Vector3d)p["position"];
+      direction = (Vector3d)p["direction"];
+      prepare();
+    }
+  }
+
+  interface IAnimatableCamera
+  {
+    string[] GetParamNames ();
+    void ApplyParams (Dictionary<string, object> p);
+  }
+}
