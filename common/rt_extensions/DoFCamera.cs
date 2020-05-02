7e99dc1fdd8cd4e6f494b8d78f862071e3ff457c
Michal Töpfer <michal.topfer@gmail.com>
2020-05-02 10:49:42 +0200

    added circle mode with uniform random jittering

--- common/rt_extensions/DoFCamera.cs
+++ common/rt_extensions/DoFCamera.cs
@@ -5,7 +5,7 @@ namespace Rendering
 {
     public class DoFCamera : StaticCamera, ICamera
     {
-        public enum ShiftMode { Circle, RandomNormal, RandomUniform };
+        public enum ShiftMode { Circle, RandomNormal, RandomUniform, CircleJitter };
         const int POINTS_ON_CIRCLE = 12;
 
         protected double focus_distance;
@@ -13,7 +13,7 @@ namespace Rendering
         protected ShiftMode shift_mode;
         protected Vector3d shift_dir;
 
-        public DoFCamera(Vector3d cen, Vector3d dir, double ang, double focus_distance = 1.0, double max_shift = 0.05, ShiftMode shift_mode = ShiftMode.Circle) : base(cen, dir, ang)
+        public DoFCamera(Vector3d cen, Vector3d dir, double ang, double focus_distance = 1.0, double max_shift = 0.03, ShiftMode shift_mode = ShiftMode.CircleJitter) : base(cen, dir, ang)
         {
             this.focus_distance = focus_distance;
             this.max_shift = max_shift;
@@ -49,26 +49,28 @@ namespace Rendering
             switch (shift_mode)
             {
                 case ShiftMode.RandomUniform:
-                    return GetShift_RandomUniform();
+                    return GetShift_RandomUniform(max_shift);
                 case ShiftMode.RandomNormal:
-                    return GetShift_RandomNormal();
+                    return GetShift_RandomNormal(max_shift);
                 case ShiftMode.Circle:
+                    return GetShift_Circle(max_shift);
+                case ShiftMode.CircleJitter:
                 default:
-                    return GetShift_Circle();
+                    return GetShift_CircleJitter(max_shift);
             }
         }
 
-        protected Vector3d GetShift_RandomUniform()
+        protected Vector3d GetShift_RandomUniform(double max_shift)
         {
             return max_shift * new Vector3d(MT.rnd.UniformNumber() - 0.5, MT.rnd.UniformNumber() - 0.5, MT.rnd.UniformNumber() - 0.5);
         }
 
-        protected Vector3d GetShift_RandomNormal ()
+        protected Vector3d GetShift_RandomNormal (double max_shift)
         {
             return max_shift * new Vector3d(MT.rnd.Normal(0, 1), MT.rnd.Normal(0, 1), MT.rnd.Normal(0, 1));
         }
 
-        protected Vector3d GetShift_Circle ()
+        protected Vector3d GetShift_Circle (double max_shift)
         {
             if (MT.rank == 0)
                 return Vector3d.Zero;
@@ -84,6 +86,16 @@ namespace Rendering
             Matrix4d rotation = Matrix4d.Rotate(direction, angle);
             Vector3d shift_direction = Vector3d.Transform(shift_dir, rotation);
             return shift_direction * distance;
+        }
+
+        protected Vector3d GetShift_CircleJitter (double max_shift)
+        {
+            if (MT.rank == 0)
+                return Vector3d.Zero;
+
+            int num_circles = (MT.total - 2) / POINTS_ON_CIRCLE + 1;
+            double jitter = 1.0 / (num_circles + 1);
+            return GetShift_Circle(max_shift) + GetShift_RandomUniform(max_shift * jitter);
         }
     }
 }
