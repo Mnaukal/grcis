28332c184319c58221def11d8e5cdd06a8bc152f
David Šosvald <david.sosvald@email.cz>
2020-06-14 20:25:33 +0200

    Animation script cleanup and tweaks.

--- final animation/animation.cs
+++ final animation/animation.cs
@@ -29,7 +29,7 @@ root.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(new double[] { 1.0, 0
 scene.Intersectable = root;
 
 // Background color:
-scene.BackgroundColor = new double[] { 0.0, 0.01, 0.03 };
+scene.BackgroundColor = new double[] { 0.02, 0.04, 0.06 };
 scene.Background = new StarBackground(scene.BackgroundColor, 1000, 0.02, 0.5, 4.0, 2.5, 42);
 
 // Light sources:
@@ -46,14 +46,10 @@ scene.Camera = new KeyframesAnimatedDoFCamera(a);
 Dictionary<string, string> p = Util.ParseKeyValueList(param);
 
 // materials
-//var red = new double[] { 0.8, 0.1, 0.1 };
-//var green = new double[] { 0.1, 1.0, 0.2 };
-//var yellow = new double[] { 0.9, 0.7, 0.1 };
 var darkBlue = new double[] { 0.0, 0.309, 0.619 };
 var purple = new double[] { 0.211, 0.0, 0.529 };
 var darkRed = new double[] { 0.623, 0.184, 0.184 };
 
-//PhongMaterial r = new PhongMaterial(red, 0.1, 0.8, 0.1, 16);
 PhongMaterial r = new PhongMaterial(darkBlue, 0.6, 0.4, 0.1, 16);
 PhongMaterial g = new PhongMaterial(purple, 0.6, 0.4, 0.1, 16);
 PhongMaterial y = new PhongMaterial(darkRed, 0.6, 0.4, 0.1, 16);
@@ -71,12 +67,6 @@ private PhongMaterial GetMaterial(int j)
     }
     return null;
 }
-
-// Base plane
-//Plane pl = new Plane();
-//pl.SetAttribute(PropertyName.COLOR, new double[] { 0.6, 0.0, 0.0 });
-//pl.SetAttribute(PropertyName.TEXTURE, new CheckerTexture(0.5, 0.5, new double[] { 1.0, 1.0, 1.0 }));
-//root.InsertChild(pl, Matrix4d.RotateX(-MathHelper.PiOver2) * Matrix4d.CreateTranslation(0.0, -1.0, 0.0));
 
 // Cubes
 for (int i = 0; i < 5; i += 1)
@@ -100,89 +90,9 @@ for (int i = 0; i < 5; i += 1)
 
 AnimatedNodeTransform mono = new AnimatedNodeTransform(a, "p_mono", "r_mono", null);
 Cube monolith = new Cube();
-monolith.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(new double[] { 0, 0, 0 }, 0.1, 0.1, 0.2, 128));
+monolith.SetAttribute(PropertyName.MATERIAL, new PhongMaterial(new double[] { 0, 0, 0 }, 0.06, 0.06, 0.12, 128));
 mono.InsertChild(monolith, Matrix4d.Scale(0.4, 3.6, 1.6) * Matrix4d.CreateTranslation(-0.2, -1.8, -0.8));
 root.InsertChild(mono, Matrix4d.Identity);
-
-
-
-//c = new Cube();
-//ac = new AnimatedNodeTransform(a, null, null, null);
-//ac.InsertChild(c, Matrix4d.CreateTranslation(10.0, 6.0, -5.0));
-//root.InsertChild(ac, Matrix4d.Identity);
-//c.SetAttribute(PropertyName.MATERIAL, r);
-
-//c = new Cube();
-//ac = new AnimatedNodeTransform(a, null, null, null);
-//ac.InsertChild(c, Matrix4d.CreateTranslation(10.0, 4.0, 4.0));
-//root.InsertChild(ac, Matrix4d.Identity);
-//c.SetAttribute(PropertyName.MATERIAL, g);
-
-
-
-
-
-
-//// animated cubes
-//c = new Cube();
-//ac = new AnimatedNodeTransform(a, "t1", null, "s1");
-//ac.InsertChild(c, Matrix4d.Identity);
-//root.InsertChild(ac, Matrix4d.Identity);
-//c.SetAttribute(PropertyName.MATERIAL, r);
-
-//c = new Cube();
-//ac = new AnimatedNodeTransform(a, null, "r1", null, new Vector3d(-5, -0.5, 0));
-//ac.InsertChild(c, Matrix4d.Identity);
-//root.InsertChild(ac, Matrix4d.Identity);
-//c.SetAttribute(PropertyName.MATERIAL, g);
-
-//c = new Cube();
-//ac = new AnimatedNodeTransform(a, null, "r1", null, new Vector3d(6, -0.5, 0));
-//ac.InsertChild(c, Matrix4d.Identity);
-//root.InsertChild(ac, Matrix4d.Identity);
-//c.SetAttribute(PropertyName.MATERIAL, g);
-
-//// still cubes
-//// front row - with animated material
-//AnimatedNodeMaterialColor front = new AnimatedNodeMaterialColor(a);
-//root.InsertChild(front, Matrix4d.Identity);
-//PhongMaterial m = new PhongMaterial(new double[] { 0, 0, 0 }, 0.1, 0.8, 0.1, 16);
-//front.SetAttribute(PropertyName.MATERIAL, m);
-
-//// the cubes are copied from the Cubes scene
-//c = new Cube();
-//front.InsertChild(c, Matrix4d.RotateY(0.6) * Matrix4d.CreateTranslation(-3.5, -0.8, 0.0));
-//c = new Cube();
-//front.InsertChild(c, Matrix4d.RotateY(1.2) * Matrix4d.CreateTranslation(-1.5, -0.8, 0.0));
-//c = new Cube();
-//front.InsertChild(c, Matrix4d.RotateY(1.8) * Matrix4d.CreateTranslation(0.5, -0.8, 0.0));
-//c = new Cube();
-//front.InsertChild(c, Matrix4d.RotateY(2.4) * Matrix4d.CreateTranslation(2.5, -0.8, 0.0));
-//c = new Cube();
-//front.InsertChild(c, Matrix4d.RotateY(3.0) * Matrix4d.CreateTranslation(4.5, -0.8, 0.0));
-
-//// back row - not animated
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(3.5) * Matrix4d.CreateTranslation(-4.0, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(3.0) * Matrix4d.CreateTranslation(-2.5, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(2.5) * Matrix4d.CreateTranslation(-1.0, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(2.0) * Matrix4d.CreateTranslation(0.5, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(1.5) * Matrix4d.CreateTranslation(2.0, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(1.0) * Matrix4d.CreateTranslation(3.5, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
-//c = new Cube();
-//root.InsertChild(c, Matrix4d.RotateX(0.5) * Matrix4d.CreateTranslation(5.0, 1.0, 2.0));
-//c.SetAttribute(PropertyName.MATERIAL, pm);
 
 //////////////////////////////////////////////////
 // If animator was created in this run of script, load keyframes based on parameters set during scene creation
